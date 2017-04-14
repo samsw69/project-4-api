770caa8125e1cf0adb27bc4b7ff2c43464937670
Samsw69 <sam@housenhome.co.uk>
2017-04-15 00:13:11 +0100

    auth completed and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
-  has_secure_password
-    has_many :events_created, class_name: "Event", foreign_key: "user_id"
-    has_many :products_created, class_name: "Product", foreign_key: "user_id"
-    
-    has_and_belongs_to_many :events_attending, class_name: "Event", join_table: "events_users"
-    has_many :comments
+  has_secure_password validations: false
 
-    validates :username, presence: true
-    validates :email, uniqueness: true, presence: true
+  has_many :comments
+
+  has_many :events_created, class_name: "Event", foreign_key: "user_id"
+  has_many :products_created, class_name: "Product", foreign_key: "user_id"
+
+  has_and_belongs_to_many :events_attending, class_name: "Event", join_table: "events_users"
+  has_many :comments
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true, unless: :oauth_login?
+  validates :password, presence: true, confirmation: true, unless: :oauth_login?, on: :create
+
+  def oauth_login?
+    github_id.present?
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   has_many :events_created
   has_many :events_attending
   has_many :products_created
-  has_many :comments_created
+  has_many :comments
   attributes :id, :name, :username, :email, :about, :image
 end
