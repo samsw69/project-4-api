14ecec13ccf5031fefe9599de24cf113657c73fc
Samsw69 <sam@housenhome.co.uk>
2017-04-19 23:32:28 +0100

    seeds file updated - more seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@
   ActiveRecord::Base.connection.execute("TRUNCATE #{model.table_name} RESTART IDENTITY CASCADE")
 end
 
-# collage = Genres.create!(genre: "Collage")
-# abstract = Genres.create!(genre: "Abstract")
-
 
 sam = User.create!(name: "Sam Wakefield",
 username: "samsw69",
@@ -30,11 +27,44 @@ image: File.open(File.join(Rails.root, "db/images/Moj-semi.jpg")),
 password: "password",
 password_confirmation: "password")
 
+jake = User.create!(name: "Jake Adams",
+username: "JakeyPops",
+email: "jake@ga.co",
+about: "a master of the cross stitch, I love trying all art and will be looking into knitted tree-wraps next",
+image: File.open(File.join(Rails.root, "db/images/smile_cross_stitch.jpg")),
+password: "password",
+password_confirmation: "password")
+
+buki = User.create!(name: "Buki Thomson",
+username: "BukiT",
+email: "buki@ga.co",
+about: "life drawing specialist, interested in the female form - always looking for subjects to draw.",
+image: File.open(File.join(Rails.root, "db/images/Moj-semi.jpg")),
+password: "password",
+password_confirmation: "password")
+
+alice = User.create!(name: "Alice Marshall",
+username: "Big Al",
+email: "alice@ga.co",
+about: "cross stitch - the best in town - looking for a worthy competitor in the are of armchair doilies",
+image: File.open(File.join(Rails.root, "db/images/Moj-semi.jpg")),
+password: "password",
+password_confirmation: "password")
+
+ajay = User.create!(name: "Ajay Lard",
+username: "Lardhouse",
+email: "ajay@ga.co",
+about: "specialist in all things pineapple.  Available for (pineapple) art commissions",
+image: File.open(File.join(Rails.root, "db/images/pineapple_head.jpg")),
+password: "password",
+password_confirmation: "password")
+
 e1 = Event.create!(title: "photography link-up",
 location: "FH community centre",
 date: Date.new(2017, 4, 28),
 description: "We will be heading to the Sedgemoor fields to work on some landscape photography.",
-image: File.open(File.join(Rails.root, "db/images/countryside.jpg")), user: sam, attendees: [sam])
+image: File.open(File.join(Rails.root, "db/images/countryside.jpg")), user: sam, attendees: [sam, jake, buki])
+Comment.create!(body: "will wellies be provided?", user: buki, event: e1)
 
 e2 = Event.create!(title: "mixed media art in the afternoon",
 location: "artists home",
@@ -42,8 +72,18 @@ date: Date.new(2017, 5, 20),
 description: "first meet-up to prep canvases for mixed collage.",
 image: File.open(File.join(Rails.root, "db/images/collage.jpg")), user: eden)
 
-Comment.create!(body: "testing testing again in events ...", user: sam, event: e2)
-Comment.create!(body: "testing testing again in events - number 2 ...", user: eden, event: e1)
+Comment.create!(body: "shall I bring the prosecco...", user: sam, event: e2)
+Comment.create!(body: "and some crisps...", user: eden, event: e2)
+
+e3 = Event.create!(title: "cross stitch masters - Heat 1",
+location: "Stratford town hall",
+date: Date.new(2017, 5, 27),
+description: "hotly contested competition for the cross-stitch queen - first round - kick-off at 7pm sharp!",
+image: File.open(File.join(Rails.root, "db/images/cross_stitch_comp.jpg")), user: alice, attendees: [alice, jake, buki, ajay])
+
+Comment.create!(body: "bring it on Alice - gunning for the crown ...", user: jake, event: e3)
+Comment.create!(body: "is it too late to enter guys? ...", user: buki, event: e3)
+Comment.create!(body: "Jake, it ended in tears last year.  Are you sure you can bring your A-stitch?", user: alice, event: e3)
 
 Product.create!(title: "Skullduggery",
 description: "Embellished with beads and costume jewellery and comes framed in a 60x60cm box frame.",
@@ -51,10 +91,90 @@ price: 89.99,
 image: File.open(File.join(Rails.root, "db/images/skull.jpg")),
 user: eden
 )
-
+Product.create!(title: "Armchair bouquets",
+description: "Stunning armchair protectors - hand stitched.",
+price: 9.99,
+image: File.open(File.join(Rails.root, "db/images/armchair_cross_stitch.jpg")),
+user: alice
+)
+Product.create!(title: "Rib cage Doillie",
+description: "handmade doillies with a beautiful ribcage design.",
+price: 9.99,
+image: File.open(File.join(Rails.root, "db/images/armchair_doillie.jpg")),
+user: alice
+)
+Product.create!(title: "Awkward balloon",
+description: "personal stories told through cross stitch - a boy and his balloon #awkward.",
+price: 2.99,
+image: File.open(File.join(Rails.root, "db/images/awkward_balloon.jpg")),
+user: alice
+)
 Product.create!(title: "Geisha Girl",
 description: "abstract collage of a geisha, in paper, paint and sand.",
 price: 109.99,
 image: File.open(File.join(Rails.root, "db/images/Geisha.jpg")),
+user: sam
+)
+
+Product.create!(title: "Bead art",
+description: "A vibrant colourful picture which includes a variety of coloured beads.",
+price: 15.50,
+image: File.open(File.join(Rails.root, "db/images/bead_art.jpg")),
+user: maureen
+)
+
+Product.create!(title: "a gift from Jake...",
+description: "edgy gift for a loved one - probably a female friend!",
+price: 19.99,
+image: File.open(File.join(Rails.root, "db/images/cross_stitch.jpg")),
+user: jake
+)
+
+Product.create!(title: "golden pineapple",
+description: "stunning disco-inspired pineapple.",
+price: 399.99,
+image: File.open(File.join(Rails.root, "db/images/pineapple.jpg")),
+user: ajay
+)
+
+Product.create!(title: "Coloured pencil peacock",
+description: "274x184cm piece of art which does not include a frame.",
+price: 7.00,
+image: File.open(File.join(Rails.root, "db/images/peacock.jpg")),
+user: sam
+)
+
+Product.create!(title: "Embroidery stool",
+description: "Extravagant piece of art that can be put to use everyday, varnished oak legs.",
+price: 35.00,
+image: File.open(File.join(Rails.root, "db/images/embroidery_stool.jpg")),
+user: ajay
+)
+
+Product.create!(title: "Floral letter P",
+description: "vintage look on a letter of your choose, bold colours with the dimensions being 193x261cm.",
+price: 24.99,
+image: File.open(File.join(Rails.root, "db/images/floral_letter.jpg")),
+user: maureen
+)
+
+Product.create!(title: "Newspaper girl",
+description: "Subtle collage of a girl with a illuminating crown.",
+price: 20.00,
+image: File.open(File.join(Rails.root, "db/images/newspaper_girl.jpg")),
+user: eden
+)
+
+Product.create!(title: "Oil painting",
+description: "A gorgeous picture that captures the bright light from the sunset using oil colours to create it.",
+price: 12.89,
+image: File.open(File.join(Rails.root, "db/images/water_lily.jpg")),
+user: maureen
+)
+
+Product.create!(title: "The Raven",
+description: "A picture that uses collage to create an image of a Raven.",
+price: 9.99,
+image: File.open(File.join(Rails.root, "db/images/raven.jpg")),
 user: sam
 )
