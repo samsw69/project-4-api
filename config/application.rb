15bd7f3a898566657e2db36d2b751827d9569e44
Samsw69 <sam@housenhome.co.uk>
2017-04-24 13:36:35 +0100

    push to heroku v2

--- config/application.rb
+++ config/application.rb
@@ -27,12 +27,12 @@ module Project4
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #     allow do
-    #       origins '*'
-    #       resource '*', :headers => :any, :methods => :any
-    #     end
-    #   end
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins ['localhost:7000', 'https://gentle-reaches-22555.herokuapp.com']
+          resource '*', :headers => :any, :methods => :any
+        end
+      end
 
       config.eager_load_paths << Rails.root.join('lib')
   end
