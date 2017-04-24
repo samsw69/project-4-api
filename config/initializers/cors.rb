e112bc52a922c62534edd96abdb8dbd48c754c03
Samsw69 <sam@housenhome.co.uk>
2017-04-24 09:33:00 +0100

    prep to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:7000']
+    origins ['localhost:7000', 'https://serene-plains-78897.herokuapp.com']
     resource '*', headers: :any, methods: :any
   end
 end
